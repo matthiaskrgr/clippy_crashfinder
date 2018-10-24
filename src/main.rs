415a2132c7ad5f8321c154ef739a4ff6fe77d5a3
Matthias Krüger <matthias.krueger@famsik.de>
2018-10-24 10:47:56 +0200

    cleanup and fix incremental printing

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
 use dirs::home_dir;
 use std::fs::{copy, create_dir, read_dir};
+use std::fs;
 use std::process::Command;
+use std::io::{self, Write};
 
 fn main() {
     // store build artifacts here
+    // will be cleared from time to time
     let mut target_dir = dirs::home_dir().unwrap();
     target_dir.push(".clippy_fuzzy_target_dir");
+    let target_dir = target_dir; // unmut
     if !target_dir.display().to_string().ends_with("dir") {
         // sanity
         panic!();
     }
     if !target_dir.is_dir() {
-        create_dir(&target_dir).unwrap();
+        fs::create_dir(&target_dir).unwrap();
     }
-    // get crates from here
+    // get crates to check from here
     let mut crate_archives = dirs::home_dir().unwrap();
     crate_archives.push(".cargo");
     crate_archives.push("registry");
@@ -26,56 +30,62 @@ fn main() {
     let work_dir = work_dir; // unmut
 
 
-    let crates = read_dir(crate_archives).unwrap();
+    let crates = read_dir(crate_archives).unwrap(); // all the creates from the cargo cache
 
     let mut crate_archives = Vec::new();
-    for c in crates {
-        let u = c.unwrap();
-        let p = u.path();
-        crate_archives.push(p);
+    for cr in crates {
+        let unwrapped = cr.unwrap();
+        let path = unwrapped.path();
+        crate_archives.push(path);
     }
 
-    crate_archives.sort();
+    crate_archives.sort(); // sort paths alphabetically
 
     // every 100 crates, remove the target dir
-    let mut target_dir_counter = 0;
+    let mut target_dir_counter: u32 = 0;
 
-    for entry in crate_archives {
+    let mut crate_counter: u32 = 0;
+
+    for archive in crate_archives {
         target_dir_counter += 1;
+        crate_counter += 1;
         // create workdir if it does not exist
         if !work_dir.is_dir() {
-            create_dir(&work_dir).unwrap();
+            fs::create_dir(&work_dir).unwrap();
         }
 
-        let archive = entry;
-
-        let copy_from = archive.clone();
-        let mut copy_to = work_dir.clone();
+        // copy the crate from cache into work dir
+        let copy_source = &archive;
+        let mut copy_dest = work_dir.clone();
+        // the filename of the crate
         let archive_name = archive.iter().last().unwrap(); // heapsize-0.4.2.crate
-        copy_to.push(&archive_name);
-        //println!("coyping {:?} to {:?}", copy_from, copy_to);
-        // copy crate to workdir
-        copy(&copy_from, &copy_to).unwrap();
+        copy_dest.push(&archive_name);
+        // println!("coyping {:?} to {:?}", copy_source, copy_dest);
+        // copy the crate to workdir
+        fs::copy(&copy_source, &copy_dest).unwrap();
 
         // extract the .crate
         // @TODO make this pure rust
         let tar = std::process::Command::new("tar")
             .arg("-xvzf")
-            .arg(&copy_to)
+            .arg(&copy_dest)
             .current_dir(&work_dir)
             .output();
         let _ = tar.unwrap();
-        let mut crate_dir = archive_name.to_string_lossy().to_string();
-        // the dir with the extracted sources
-        let crate_name = crate_dir.replace(".crate", "");
-        let name = crate_name.clone();
-        let mut crate_dir = work_dir.clone();
-        crate_dir.push(crate_name);
-    //    println!("CD {:?}", crate_dir);
-        println!("{}", name);
-        let CLIPPY = std::process::Command::new("cargo")
-            .arg("clippy")
 
+        // the dir with the extracted sources
+        let crate_file_name = archive_name.to_string_lossy().to_string();
+        //println!("crate name: {}", crate_file_name);
+
+        let crate_name = crate_file_name.replace(".crate", "");
+
+        let mut crate_dir = work_dir.clone();
+        crate_dir.push(&crate_name);
+    //    println!("CD {:?}", crate_dir);
+        print!("{:>4} Checking {}", crate_counter, crate_name,);
+        std::io::stdout().flush().unwrap();
+        let clippy = std::process::Command::new("cargo")
+            .arg("clippy")
             .arg("--all-targets")
             .arg("--all-features")
             .arg("-vvvv")
@@ -115,12 +125,12 @@ fn main() {
             .output().unwrap();
         //println!("crate_dir: {}, cargo_target_dir {}", crate_dir, target_dir.display());
         //println!("output: {:?}", CLIPPY);
-        let stderr = String::from_utf8_lossy(&CLIPPY.stderr);
-        let stdout = String::from_utf8_lossy(&CLIPPY.stdout);
+        let stderr = String::from_utf8_lossy(&clippy.stderr);
+        let stdout = String::from_utf8_lossy(&clippy.stdout);
         if stderr.contains("internal compiler error") || stderr.contains("query stack during panic") || stdout.contains("internal compiler error") || stdout.contains("query stack during panic") {
-            println!("{} ERROR ERROR ERROR!", name);
+            println!(" ERROR");
         } else {
-            println!("{} ok", name);
+            println!(" ok");
         }
 
         //println!("{}", stderr);
